433e70a8d6c2ed66c40792a58dce4267f0261f3a
Hans Bakker <hansmbakker+github@gmail.com>
2017-02-04 20:44:16 +0100

    Add project files.

---
+++ testApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using Broadlink.Net;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var client = new Client();
+        var discoveredDevices = client.DiscoverAsync().Result;
+        
+        if (discoveredDevices.Any())
+        {
+            var deviceToUse = discoveredDevices.FirstOrDefault();
+            if(deviceToUse != null)
+            {
+                var rmDevice = deviceToUse as RMDevice;
+                rmDevice.AuthorizeAsync().Wait();
+                var temp = rmDevice.GetTemperatureAsync().Result;
+                rmDevice.EnterLearningModeAsync().Wait();
+                var data = rmDevice.ReadLearningDataAsync().Result;
+                Console.WriteLine(BitConverter.ToString(data));
+                rmDevice.SendRemoteCommandAsync(data).Wait();
+            }
+            
+        }
+        Console.ReadLine();
+    }
+}
